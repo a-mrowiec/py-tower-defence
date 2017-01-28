ff7b7a402dfd3b4c2c1c034128eee65c243d43ff
Adrian Mrowiec <admr@kipertech.pl>
2017-01-28 18:49:26 +0100

    Cleanup

--- pytowerdefence/gameplay/Monsters.py
+++ pytowerdefence/gameplay/Monsters.py
@@ -4,69 +4,15 @@ from pytowerdefence.gameplay.Controllers import PathController, DeathController,
     RangeAttackController, AttackController
 from pytowerdefence.gameplay.Objects import Actor, ActorState
 
-rects = [(0, 154, 94, 77),
-         (94, 154, 94, 77),
-         (188, 154, 94, 77),
-         (282, 154, 94, 77),
-         (376, 154, 94, 77),
-         (470, 154, 94, 77),
-         (564, 154, 94, 77),
-         (658, 154, 94, 77),
-         (752, 154, 94, 77), ]
-
-ogre_move_rects = [(0, 0, 64, 64),
-                   (64, 0, 64, 64),
-                   (128, 0, 64, 64),
-                   (192, 0, 64, 64),
-                   (256, 0, 64, 64),
-                   (320, 0, 64, 64),
-                   (384, 0, 64, 64),
-                   (448, 0, 64, 64), ]
-
-ogre_idle_rects = [(0, 0, 64, 64)]
-
-ogre_death_rects = [(0, 0, 64, 64),
-                    (64, 0, 64, 64),
-                    (128, 0, 64, 64),
-                    (192, 0, 64, 64),
-                    (0, 64, 64, 64),
-                    (64, 64, 64, 64),
-                    (128, 64, 64, 64),
-                    (192, 64, 64, 64),
-                    (0, 128, 64, 64),
-                    (64, 128, 64, 64),
-                    (128, 128, 64, 64),
-                    (192, 128, 64, 64),
-                    (0, 192, 64, 64),
-                    (64, 192, 64, 64),
-                    (128, 192, 64, 64),
-                    (192, 192, 64, 64),
-                    (0, 256, 64, 64),
-                    (64, 256, 64, 64),
-                    (128, 256, 64, 64),
-                    (192, 256, 64, 64),
-                    (0, 320, 64, 64),
-                    (64, 320, 64, 64),
-                    (128, 320, 64, 64),
-                    (192, 320, 64, 64),
-                    (0, 384, 64, 64),
-                    (64, 384, 64, 64),
-                    (128, 384, 64, 64),
-                    (192, 384, 64, 64),
-                    (0, 448, 64, 64),
-                    (64, 448, 64, 64),
-                    (128, 448, 64, 64),
-                    (192, 448, 64, 64)]
-
 
 class Ogre(Actor):
     def __init__(self):
         super().__init__()
         self._statistics.speed = 50
 
-        self.set_animation(ActorState.IDLE,ResourceManager.load_animation(
+        self.set_animation(ActorState.IDLE, ResourceManager.load_animation(
             ResourceClass.CHARACTERS, 'ogre-move.json'))
-        self.set_animation(ActorState.MOVE,ResourceManager.load_animation(
+        self.set_animation(ActorState.MOVE, ResourceManager.load_animation(
             ResourceClass.CHARACTERS, 'ogre-move.json'))
         self.set_animation(ActorState.ATTACK, ResourceManager.load_animation(
             ResourceClass.CHARACTERS, 'ogre-attack.json'))
