a063013a39784d18f5391a80b0d91d7ee2ce05ca
Adrian Mrowiec <admr@kipertech.pl>
2017-02-05 00:53:43 +0100

    Pylint

--- pytowerdefence/mainmenu/MainMenuPhase.py
+++ pytowerdefence/mainmenu/MainMenuPhase.py
@@ -1,3 +1,6 @@
+"""
+Main menu phase
+"""
 import pygame
 from pygame.math import Vector2
 
@@ -7,15 +10,19 @@ from pytowerdefence.UI import Button, PositionAttachType, Panel
 
 
 class MainMenuPhase(Phase):
+    """
+    Main menu phase
+    """
+
     def __init__(self, app, ui_manager):
         self._app = app
         self._ui_manager = ui_manager
 
     def initialise(self, **kwargs):
         image = ResourceManager.load_image(ResourceClass.UI, 'main-menu-bg.jpg')
-        panel=Panel(img=pygame.transform.scale(image, (1024, 768)))
+        panel = Panel(img=pygame.transform.scale(image, (1024, 768)))
         start_button = Button("Start Game", color=(255, 255, 255), size=44)
-        start_button._click_callback = self.start_game
+        start_button.click_callback = self.start_game
         start_button.position_attach_type = PositionAttachType.CENTER
         start_button.position = Vector2(self._ui_manager.window_size.x / 2,
                                         self._ui_manager.window_size.y / 2)
@@ -24,7 +31,12 @@ class MainMenuPhase(Phase):
         self._ui_manager.add_widget(start_button)
         self._ui_manager.add_widget(panel)
 
-    def start_game(self, event):
+    def start_game(self, _):
+        """
+        Starts game
+        :param _:
+        :return:
+        """
         self._app.set_phase('game', filename='data/maps/1.json')
 
     def on_destroy(self):
