7b66337aaa4925711fe9bb84d10e027a97024f5f
Adrian Mrowiec <admr@kipertech.pl>
2017-02-04 20:25:04 +0100

    Menu glowne

--- pytowerdefence/mainmenu/MainMenuPhase.py
+++ pytowerdefence/mainmenu/MainMenuPhase.py
@@ -1,7 +1,9 @@
+import pygame
 from pygame.math import Vector2
 
 from pytowerdefence.Phase import Phase
-from pytowerdefence.UI import Button, PositionAttachType
+from pytowerdefence.Resources import ResourceManager, ResourceClass
+from pytowerdefence.UI import Button, PositionAttachType, Panel
 
 
 class MainMenuPhase(Phase):
@@ -10,12 +12,17 @@ class MainMenuPhase(Phase):
         self._ui_manager = ui_manager
 
     def initialise(self, **kwargs):
+        image = ResourceManager.load_image(ResourceClass.UI, 'main-menu-bg.jpg')
+        panel=Panel(img=pygame.transform.scale(image, (1024, 768)))
         start_button = Button("Start Game", color=(255, 255, 255), size=44)
         start_button._click_callback = self.start_game
         start_button.position_attach_type = PositionAttachType.CENTER
         start_button.position = Vector2(self._ui_manager.window_size.x / 2,
                                         self._ui_manager.window_size.y / 2)
+        start_button.z = 1
+        panel.z = 0
         self._ui_manager.add_widget(start_button)
+        self._ui_manager.add_widget(panel)
 
     def start_game(self, event):
         self._app.set_phase('game', map_file='data/maps/test.tmx')
