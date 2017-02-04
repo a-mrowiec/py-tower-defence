7e4489d715c638d42fafa6fb4a2bf7560152b8af
Adrian Mrowiec <admr@kipertech.pl>
2017-02-04 15:56:45 +0100

    Dodanie wody do mapy, lekki refaktor

--- pytowerdefence/gameplay/Monsters.py
+++ pytowerdefence/gameplay/Monsters.py
@@ -1,4 +1,5 @@
 import copy
+
 from pytowerdefence.Resources import ResourceClass, ResourceManager
 from pytowerdefence.gameplay.AI import StandardAI
 from pytowerdefence.gameplay.Controllers import PathController, DeathController, \
@@ -7,24 +8,27 @@ from pytowerdefence.gameplay.Objects import Actor, ActorState, EvolvingActor, \
     Monster
 
 
+def set_animations(actor, animation_name_prefix):
+    actor.set_animation(ActorState.IDLE, ResourceManager.load_animation(
+        ResourceClass.CHARACTERS, animation_name_prefix + '-idle.json'))
+    actor.set_animation(ActorState.MOVE, ResourceManager.load_animation(
+        ResourceClass.CHARACTERS, animation_name_prefix + '-move.json'))
+    actor.set_animation(ActorState.ATTACK, ResourceManager.load_animation(
+        ResourceClass.CHARACTERS, animation_name_prefix + '-attack.json'))
+    actor.set_animation(ActorState.DEATH, ResourceManager.load_animation(
+        ResourceClass.CHARACTERS, animation_name_prefix + '-death.json'))
+
+
 class Ogre(Monster):
     def __init__(self):
         super().__init__(gold_gain=50)
 
-        self.set_animation(ActorState.IDLE, ResourceManager.load_animation(
-            ResourceClass.CHARACTERS, 'ogre-move.json'))
-        self.set_animation(ActorState.MOVE, ResourceManager.load_animation(
-            ResourceClass.CHARACTERS, 'ogre-move.json'))
-        self.set_animation(ActorState.ATTACK, ResourceManager.load_animation(
-            ResourceClass.CHARACTERS, 'ogre-attack.json'))
-        self.set_animation(ActorState.DEATH, ResourceManager.load_animation(
-            ResourceClass.CHARACTERS, 'ogre-death.json'))
+        set_animations(self, 'ogre')
 
         self.base_statistics.speed = 50
         self.base_statistics.attack_range = 2
-        # self.base_statistics.attack_damage = 1
         self.base_statistics.max_health = self.hp = 100
-        self.base_statistics.hit_effects =[('HitEffect', {'damage': 1})]
+        self.base_statistics.hit_effects = [('HitEffect', {'damage': 1})]
         self.recalculate_statistics()
         self.change_state(ActorState.MOVE)
 
@@ -40,20 +44,13 @@ class Dragon(Monster):
     def __init__(self):
         super().__init__(gold_gain=500)
 
-        self.set_animation(ActorState.IDLE, ResourceManager.load_animation(
-            ResourceClass.CHARACTERS, 'dragon-move.json'))
-        self.set_animation(ActorState.MOVE, ResourceManager.load_animation(
-            ResourceClass.CHARACTERS, 'dragon-move.json'))
-        self.set_animation(ActorState.ATTACK, ResourceManager.load_animation(
-            ResourceClass.CHARACTERS, 'dragon-attack.json'))
-        self.set_animation(ActorState.DEATH, ResourceManager.load_animation(
-            ResourceClass.CHARACTERS, 'dragon-death.json'))
+        set_animations(self, 'dragon')
 
         self.base_statistics.speed = 50
         self.base_statistics.attack_range = 30
         self.base_statistics.bullet_image = 'flaming-arrow.png'
         self.base_statistics.bullet_speed = 200
-        self.base_statistics.hit_effects =[('HitEffect', {'damage': 1})]
+        self.base_statistics.hit_effects = [('HitEffect', {'damage': 1})]
         self.base_statistics.max_health = self.hp = 1000
         self.recalculate_statistics()
         self.change_state(ActorState.MOVE)
@@ -66,22 +63,21 @@ class Dragon(Monster):
         self.set_ai(StandardAI())
 
 
+class Base(Actor):
+    def __init__(self):
+        super().__init__()
+        set_animations(self, 'base')
+
+
 class Bandit(EvolvingActor):
     def __init__(self):
         super().__init__()
 
-        self.set_animation(ActorState.MOVE, ResourceManager.load_animation(
-            ResourceClass.CHARACTERS, 'bandit-move.json'))
-        self.set_animation(ActorState.IDLE, ResourceManager.load_animation(
-            ResourceClass.CHARACTERS, 'bandit-idle.json'))
-        self.set_animation(ActorState.ATTACK, ResourceManager.load_animation(
-            ResourceClass.CHARACTERS, 'bandit-attack.json'))
-        self.set_animation(ActorState.DEATH, ResourceManager.load_animation(
-            ResourceClass.CHARACTERS, 'bandit-death.json'))
+        set_animations(self, 'bandit')
 
         self.base_statistics.speed = 50
         self.base_statistics.attack_range = 100
-        self.base_statistics.hit_effects =[('HitEffect', {'damage': 15})]
+        self.base_statistics.hit_effects = [('HitEffect', {'damage': 15})]
         self.base_statistics.bullet_speed = 100
         self.base_statistics.bullet_image = 'small-knife.png'
         self.base_statistics.max_health = self.hp = 100
@@ -96,12 +92,13 @@ class Bandit(EvolvingActor):
 
         self.set_ai(StandardAI(debug=True))
 
-        stats=copy.deepcopy(self._base_statistics)
-        stats.hit_effects = [('HitEffect', {'damage': 30}), ('SlowEffect', {'time': 5, 'percent': 0.5})]
+        stats = copy.deepcopy(self._base_statistics)
+        stats.hit_effects = [('HitEffect', {'damage': 30}),
+                             ('SlowEffect', {'time': 5, 'percent': 0.5})]
         stats.bullet_speed = 500
         self.add_evolution_level(stats, 50, None)
 
-        stats=copy.deepcopy(stats)
+        stats = copy.deepcopy(stats)
         stats.attack_range += 200
         stats.bullet_speed = 1000
         stats.bullet_image = 'flaming-arrow.png'
